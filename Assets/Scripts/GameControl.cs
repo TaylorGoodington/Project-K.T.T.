7ae568d28a68351c20b6fa9e4f202a2d412716ef
Taylor Goodwin <taylor@goodkingstudios.com>
2017-01-14 20:20:59 -0500

    Initial commit.

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+    private static GameControl _thisObject;
+
+    void Awake()
+    {
+        if (_thisObject != null)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+        _thisObject = this;
+        DontDestroyOnLoad(transform.root.gameObject);
+    }
+
+    public static void GoToLevel (string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
---
+++ Assets/Scripts/Main/StartController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class StartController : MonoBehaviour
+{
+    private Animator animator;
+    private string levelDestination;
+
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+	}
+
+    public void TransitionOut (string destination)
+    {
+        levelDestination = destination;
+        animator.Play("transitionOut");
+    }
+
+    public void CallGoToLevel ()
+    {
+        GameControl.GoToLevel(levelDestination);
+    }
+}
